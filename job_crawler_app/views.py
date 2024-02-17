6814a40c4c12420d13937853fcd01b7c71cbca46
muasya-victor <vicmwe184@gmail.com>
2024-02-17 14:22:33 +0300

    - correct urls

--- job_crawler_app/views.py
+++ job_crawler_app/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
