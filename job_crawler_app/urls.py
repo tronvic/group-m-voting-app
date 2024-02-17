7ab9c78bfac7c17e097ee3e4fd4e4350c2c0ed97
muasya-victor <vicmwe184@gmail.com>
2024-02-17 19:11:59 +0300

    - import layout Throughout

--- job_crawler_app/urls.py
+++ job_crawler_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("history", views.jobHistoryView, name="history"),
+    path("portfolio", views.portfolioView, name="portfolio"),
+    path("job-request", views.requestJobView, name="request"),
 ]
--- job_crawler_app/views.py
+++ job_crawler_app/views.py
@@ -5,5 +5,15 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('home.html')
+    template = loader.get_template('pages/landing.html')
+    return HttpResponse(template.render())
+
+def jobHistoryView(request):
+    template = loader.get_template('pages/job_history.html')
+    return HttpResponse(template.render())
+def portfolioView(request):
+    template = loader.get_template('pages/portfolio.html')
+    return HttpResponse(template.render())
+def requestJobView(request):
+    template = loader.get_template('pages/job_request.html')
     return HttpResponse(template.render())
